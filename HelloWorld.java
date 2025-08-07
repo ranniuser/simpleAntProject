ec865ec6ecc7129f7725738ede9ef3a9ea23b39c
exploreGi <rannichat@gmail.com>
2025-08-07 10:24:28 -0400

    merge conflich example:

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hi Hello there");
-        System.out.println("How are you???");
+        System.out.println("How are you doing???");
     }
